5e01678660da42227f10d9bc9611852a45aadd0d
Konstantin1spb <105925642+sitepreview@users.noreply.github.com>
2024-11-21 21:22:05 +0300

    feat: useHover

--- src/hooks/useHover.js
+++ src/hooks/useHover.js
@@ -1,3 +1,28 @@
 import { useEffect, useRef, useState } from 'react';
 
-export const useHover = () => {};
+export const useHover = () => {
+	const [hovered, setHovered] = useState(false);
+	const ref = useRef();
+
+	useEffect(() => {
+		const element = ref.current;
+		console.log(element);
+
+		element.addEventListener('mouseenter', () => {
+			setHovered(true);
+		});
+		element.addEventListener('mouseleave', () => {
+			setHovered(false);
+		});
+
+		return () => {
+			element.removeEventListener('mouseenter'); // удалится?
+			element.removeEventListener('mouseleave');
+		};
+	}, []);
+
+	return {
+		hovered,
+		ref,
+	};
+};
